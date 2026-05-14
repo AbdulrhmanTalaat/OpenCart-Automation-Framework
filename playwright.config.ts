2351b008513a067f061ea1db7f467f0cfb2c6f69
Abdulrhman Talaat <abdulrhmantalaat9@gmail.com>
2026-05-14 12:55:40 +0300

    Run End to End Senario Generated  ./allure-report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,43 +1,43 @@
 import { defineConfig, devices } from '@playwright/test';
- export default defineConfig({
+export default defineConfig({
 
-    timeout: 30 * 1000, //30000 ms(30 secs) 
-    testDir: './tests',
-    fullyParallel: false, 
-    //retries: process.env.CI ? 2 : 0,
-    //workers: process.env.CI ? 1 : undefined, 
-    workers: 1, 
-     //retries: 2,
+   timeout: 30 * 1000, //30000 ms(30 secs) 
+   testDir: './tests',
+   fullyParallel: false,
+   //retries: process.env.CI ? 2 : 0,
+   //workers: process.env.CI ? 1 : undefined, 
+   workers: 1,
+   //retries: 2,
 
 
 
-   reporter: [ 
-      ['html'], 
-   //['allure-playwright'],
-   // ['dot'], 
+   reporter: [
+      ['html'],
+      ['allure-playwright'],
+    //  ['dot'],
       ['list']
-  ],
+   ],
 
 
-  use: {
-     trace: 'on-first-retry',
-     screenshot: 'on',
-     video: 'on', 
+   use: {
+      trace: 'on-first-retry',
+      screenshot: 'on',
+      video: 'on',
 
       //headless: false, 
       viewport: { width: 1280, height: 720 }, // Set default viewport size for consistency 
       ignoreHTTPSErrors: true, // Ignore SSL errors if necessary 
       permissions: ['geolocation'],  // Set necessary permissions for geolocation-based tests
-      },
+   },
 
 
-      //grep: /@master/, 
+   //grep: /@master/, 
 
-       projects: [ 
-              { name: 'chromium', use: { ...devices['Desktop Chrome'] },}, 
+   projects: [
+      { name: 'chromium', use: { ...devices['Desktop Chrome'] }, },
 
-              /* { name: 'firefox',use: { ...devices['Desktop Firefox'] },}, 
-              { name: 'webkit', use:{ ...devices['Desktop Safari'] }, } */
-        ],
-                  
-  });
+      /* { name: 'firefox',use: { ...devices['Desktop Firefox'] },}, 
+      { name: 'webkit', use:{ ...devices['Desktop Safari'] }, } */
+   ],
+
+});
